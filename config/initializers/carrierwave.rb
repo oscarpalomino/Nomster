7aed27a6f0c7f3a1659d412808f829a68b4c2160
oscar palomino <oscarpalomino11@yahoo.com>
2019-10-23 20:02:17 +0000

    adjusting the mailer to html and text.erb files

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_public = false
-  config.fog_credentials - {
+  config.fog_credentials = {
     provider:               'AWS',                       # required
     aws_access_key_id:      ENV["AWS_ACCESS_KEY"],       # required
     aws_secret_access_key:  ENV["AWS_SECRET_KEY"],       # required
